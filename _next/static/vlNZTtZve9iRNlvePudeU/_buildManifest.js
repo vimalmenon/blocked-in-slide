bdd4158ab53bef8285d565ece5ae05f71bd56d47
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-06-08 18:11:54 +0800

    update slide

--- _next/static/vlNZTtZve9iRNlvePudeU/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/0b3bc6d9fb70af55.css","static/chunks/pages/index-d4253f38c4e6e674.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
