326f787aecc7a5013267699efb17451b37145414
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-06-08 17:48:47 +0800

    Added ContactUS

--- src/deck/slides/Slide4.tsx
+++ src/deck/slides/Slide4.tsx
@@ -5,7 +5,10 @@ export const Slide4: React.FC = () => {
   return (
     <section>
       <BlockedIn />
+      <div>Demo</div>
       <div>Contact US</div>
+      <p>LinkedIn</p>
+      <p>YouTube</p>
     </section>
   );
 };
