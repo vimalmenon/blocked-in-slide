24c5ed86c2d55ea8f68cfdbd0139c16d6cb3ea02
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-06-09 19:20:50 +0800

    working on slide

---
+++ _next/static/mo23bSV1qHys_Y-XWES-A/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/0b3bc6d9fb70af55.css","static/chunks/pages/index-313f4c7cac239df9.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
