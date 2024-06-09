754c16c67914198f8ddc6fc9a5f30eb7abb10c8e
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-06-09 17:44:13 +0800

    update slides

--- src/deck/slides/Slide2.tsx
+++ src/deck/slides/Slide2.tsx
@@ -3,76 +3,78 @@ import { BlockedIn } from '../../common';
 
 export const Slide2: React.FC = () => {
   return (
-    <section className="flex flex-col gap-2">
-      <span className="mb-10">
-        <BlockedIn />
-      </span>
-      <div className="mb-2" style={{ fontSize: '25px' }}>
-        Problem application is trying to solve
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
+    <section>
+      <div className="flex flex-col gap-2">
+        <span className="mb-10">
+          <BlockedIn />
+        </span>
+        <div className="mb-2" style={{ fontSize: '25px' }}>
+          Problem application is trying to solve
+        </div>
         <div
           style={{
             display: 'flex',
-            width: '500px',
             flexDirection: 'column',
-            alignItems: 'start',
-            gap: 20,
+            justifyContent: 'center',
+            alignItems: 'center',
           }}
         >
-          <ul>
-            <li
-              className="fragment fade-up text-sm m-5"
-              style={{ fontSize: '20px' }}
-            >
-              Transparency with employment history
-            </li>
-            <li
-              className="fragment fade-up text-sm m-5"
-              style={{ fontSize: '20px' }}
-            >
-              A centralized application for all employee data
-            </li>
-            <li
-              className="fragment fade-up text-sm m-5"
-              style={{ fontSize: '20px' }}
-            >
-              Standardize the hiring process, which will help with the
-              automation
-            </li>
-            <li
-              className="fragment fade-up text-sm m-5"
-              style={{ fontSize: '20px' }}
-            >
-              Reduce the cost and time to verify employees&apos;
-            </li>
-            <li
-              className="fragment fade-up text-sm m-5"
-              style={{ fontSize: '20px' }}
-            >
-              Prevents malpractices by employees like Moonlighting and many
-              other
-            </li>
-            <li
-              className="fragment fade-up text-sm m-5"
-              style={{ fontSize: '20px' }}
-            >
-              Help employers track employees&apos; status from counter offers
-            </li>
-            <li
-              className="fragment fade-up text-sm m-5"
-              style={{ fontSize: '20px' }}
-            >
-              Tokenizing Talent
-            </li>
-          </ul>
+          <div
+            style={{
+              display: 'flex',
+              width: '500px',
+              flexDirection: 'column',
+              alignItems: 'start',
+              gap: 20,
+            }}
+          >
+            <ul>
+              <li
+                className="fragment fade-up text-sm m-5"
+                style={{ fontSize: '20px' }}
+              >
+                Transparency with employment history
+              </li>
+              <li
+                className="fragment fade-up text-sm m-5"
+                style={{ fontSize: '20px' }}
+              >
+                A centralized application for all employee data
+              </li>
+              <li
+                className="fragment fade-up text-sm m-5"
+                style={{ fontSize: '20px' }}
+              >
+                Standardize the hiring process, which will help with the
+                automation
+              </li>
+              <li
+                className="fragment fade-up text-sm m-5"
+                style={{ fontSize: '20px' }}
+              >
+                Reduce the cost and time to verify employees&apos;
+              </li>
+              <li
+                className="fragment fade-up text-sm m-5"
+                style={{ fontSize: '20px' }}
+              >
+                Prevents malpractices by employees like Moonlighting and many
+                other
+              </li>
+              <li
+                className="fragment fade-up text-sm m-5"
+                style={{ fontSize: '20px' }}
+              >
+                Help employers track employees&apos; status from counter offers
+              </li>
+              <li
+                className="fragment fade-up text-sm m-5"
+                style={{ fontSize: '20px' }}
+              >
+                Tokenizing Talent
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </section>
