09cd2ddbb39cf5232bdbac93bfe6996217ed79c4
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-06-10 10:10:23 +0800

    update slide

--- src/deck/slides/Slide6.tsx
+++ src/deck/slides/Slide6.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BlockedIn } from '../../common';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedinIn, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
 export const Slide6: React.FC = () => {
   return (
     <section>
@@ -15,23 +18,33 @@ export const Slide6: React.FC = () => {
         >
           Contact US
         </div>
-        <div className="text-base subpixel-antialiased">
+        <div
+          className="text-base subpixel-antialiased"
+          style={{ margin: '15px 0' }}
+        >
           <a
             href="https://www.linkedin.com/in/vimal-menon-72ab931a/"
             target="_blank"
             rel="noreferrer"
+            style={{ color: 'white', fontSize: '1.5rem' }}
           >
-            LinkedIn
+            LinkedIn{' '}
+            <FontAwesomeIcon icon={faLinkedinIn} style={{ color: 'blue' }} />
           </a>
         </div>
 
-        <div className="text-base subpixel-antialiased">
+        <div
+          className="text-base subpixel-antialiased"
+          style={{ margin: '15px 0' }}
+        >
           <a
             href="https://www.youtube.com/@i_am_vimalmenon"
             target="_blank"
             rel="noreferrer"
+            style={{ color: 'white', fontSize: '1.5rem' }}
           >
-            YouTube
+            YouTube{' '}
+            <FontAwesomeIcon icon={faYoutube} style={{ color: 'red' }} />
           </a>
         </div>
       </div>
